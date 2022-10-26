96ac9e49e6448cb923db8f158c0ded9ea7a80ccb
LacyAttorney599 <clem.blaizel@outlook.fr>
2022-10-26 15:31:01 +0200

    Update SongComponent.vue

--- PortFolio/src/components/SongComponent.vue
+++ PortFolio/src/components/SongComponent.vue
@@ -7,73 +7,100 @@ export default {
 <template>
     <div class="songList">
         <div class="blockSong">
-            <i class="bi bi-play-circle-fill"></i>
+            <i id="logoPlay" class="bi bi-play-circle-fill"></i>
+            <span><img class="imgAlbum"
+                    src="https://www.thebackpackerz.com/wp-content/uploads/2018/09/Josman-400x400.jpg" width="15px"
+                    height="15px" alt=""></span>
             <span class="songParams">Titre</span>
             <span class="songParams">Artiste</span>
             <span class="songParams">Album</span>
         </div>
 
         <div class="blockSong">
-            <i class="bi bi-play-circle-fill"></i>
+            <i id="logoPlay" class="bi bi-play-circle-fill"></i>
+            <span><img class="imgAlbum"
+                    src="https://www.thebackpackerz.com/wp-content/uploads/2018/09/Josman-400x400.jpg" width="15px"
+                    height="15px" alt=""></span>
             <span class="songParams">Titre</span>
             <span class="songParams">Artiste</span>
             <span class="songParams">Album</span>
         </div>
 
         <div class="blockSong">
-            <i class="bi bi-play-circle-fill"></i>
+            <i id="logoPlay" class="bi bi-play-circle-fill"></i>
+            <span><img class="imgAlbum"
+                    src="https://www.thebackpackerz.com/wp-content/uploads/2018/09/Josman-400x400.jpg" width="15px"
+                    height="15px" alt=""></span>
             <span class="songParams">Titre</span>
             <span class="songParams">Artiste</span>
             <span class="songParams">Album</span>
         </div>
 
         <div class="blockSong">
-            <i class="bi bi-play-circle-fill"></i>
+            <i id="logoPlay" class="bi bi-play-circle-fill"></i>
+            <span><img class="imgAlbum"
+                    src="https://www.thebackpackerz.com/wp-content/uploads/2018/09/Josman-400x400.jpg" width="15px"
+                    height="15px" alt=""></span>
             <span class="songParams">Titre</span>
             <span class="songParams">Artiste</span>
             <span class="songParams">Album</span>
         </div>
 
         <div class="blockSong">
-            <i class="bi bi-play-circle-fill"></i>
-            <img src="" alt="">
+            <i id="logoPlay" class="bi bi-play-circle-fill"></i>
+            <span><img class="imgAlbum"
+                    src="https://www.thebackpackerz.com/wp-content/uploads/2018/09/Josman-400x400.jpg" width="15px"
+                    height="15px" alt=""></span>
             <span class="songParams">Titre</span>
             <span class="songParams">Artiste</span>
             <span class="songParams">Album</span>
         </div>
 
         <div class="blockSong">
-            <i class="bi bi-play-circle-fill"></i>
+            <i id="logoPlay" class="bi bi-play-circle-fill"></i>
+            <span><img class="imgAlbum"
+                    src="https://www.thebackpackerz.com/wp-content/uploads/2018/09/Josman-400x400.jpg" width="15px"
+                    height="15px" alt=""></span>
             <span class="songParams">Titre</span>
             <span class="songParams">Artiste</span>
             <span class="songParams">Album</span>
         </div>
 
         <div class="blockSong">
-            <i class="bi bi-play-circle-fill"></i>
-            <img src="" alt="">
+            <i id="logoPlay" class="bi bi-play-circle-fill"></i>
+            <span><img class="imgAlbum"
+                    src="https://www.thebackpackerz.com/wp-content/uploads/2018/09/Josman-400x400.jpg" width="15px"
+                    height="15px" alt=""></span>
             <span class="songParams">Titre</span>
             <span class="songParams">Artiste</span>
             <span class="songParams">Album</span>
         </div>
 
         <div class="blockSong">
-            <i class="bi bi-play-circle-fill"></i>
+            <i id="logoPlay" class="bi bi-play-circle-fill"></i>
+            <span><img class="imgAlbum"
+                    src="https://www.thebackpackerz.com/wp-content/uploads/2018/09/Josman-400x400.jpg" width="15px"
+                    height="15px" alt=""></span>
             <span class="songParams">Titre</span>
             <span class="songParams">Artiste</span>
             <span class="songParams">Album</span>
         </div>
 
         <div class="blockSong">
-            <i class="bi bi-play-circle-fill"></i>
-            <img src="" alt="">
+            <i id="logoPlay" class="bi bi-play-circle-fill"></i>
+            <span><img class="imgAlbum"
+                    src="https://www.thebackpackerz.com/wp-content/uploads/2018/09/Josman-400x400.jpg" width="15px"
+                    height="15px" alt=""></span>
             <span class="songParams">Titre</span>
             <span class="songParams">Artiste</span>
             <span class="songParams">Album</span>
         </div>
 
         <div class="blockSong">
-            <i class="bi bi-play-circle-fill"></i>
+            <i id="logoPlay" class="bi bi-play-circle-fill"></i>
+            <span><img class="imgAlbum"
+                    src="https://www.thebackpackerz.com/wp-content/uploads/2018/09/Josman-400x400.jpg" width="15px"
+                    height="15px" alt=""></span>
             <span class="songParams">Titre</span>
             <span class="songParams">Artiste</span>
             <span class="songParams">Album</span>
@@ -91,9 +118,11 @@ export default {
 
 .blockSong {
     color: white;
+    gap: 10px;
     padding: 10px;
     display: block;
     border-radius: 5px;
+    height: 40px;
     font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
 }
 
@@ -103,7 +132,15 @@ export default {
 
 }
 
+#logoPlay {
+    margin-right: 25px;
+}
+
+.imgAlbum {
+    margin-right: 25px;
+}
+
 .songParams {
-    margin-right: 300px;
+    margin-right: 200px;
 }
 </style>
